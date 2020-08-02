c2e88edb44633d312562bce24ad8052431e4af34
John Sullivan <johnography@outlook.com>
2020-08-02 14:48:45 -0700

    fortune-teller-logic

--- src/components/FortuneTeller.js
+++ src/components/FortuneTeller.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FortuneTeller.css';
 
+const fortunes = [
+  'Be brave when facing the competition.',
+  'An exciting opportunity lies ahead of you.',
+  'A routine task will turn into an exchanting adventure.',
+  'Be true to your work, your word, and your friends.',
+  'You will receive money from an unexpected source.',
+  'Plan for many pleasures ahead.',
+];
+
 export default function FortuneTeller() {
+  const [fortune, setFortune] = useState('Your fortune will appear here...');
+  function getFortune() {
+    setFortune(fortunes[Math.floor(Math.random() * fortunes.length)]);
+  }
   return (
     <>
       <div className="Fortune-Teller neushadow">
-        <div className="FT-Header">
+        <div className="FT-Header neushadow">
           <div className="FT-Title">Fortunes, Forcasts, Lucky Charms</div>
-          <div className="FT-SubTitle">'Catch the dope of your horoscope!'</div>
+          <div className="FT-SubTitle">Catch the dope of your horoscope!</div>
         </div>
         <div className="FT-Main">
-          <div className="FT-Panel">Panel</div>
-          <div className="FT-Button">Button</div>
+          <div className="FT-Panel">{fortune}</div>
+          <div className="FT-Button" onClick={() => getFortune()}>
+            Push me!
+          </div>
         </div>
-        <div className="FT-Footer">Footer</div>
+        <div className="FT-Footer">Please come again!</div>
       </div>
     </>
   );
