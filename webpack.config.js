0944373c3a203fa44988fd855a7cd7a39ec16d01
John Sullivan <johnography@outlook.com>
2020-08-02 14:22:18 -0700

    build

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
-  // mode: 'development',
+  mode: 'production',
   module: {
     rules: [
       {
