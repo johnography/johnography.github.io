57bfda86d042501f839da29f0c5e8faaa2fe63e9
John Sullivan <johnography@outlook.com>
2020-08-02 15:46:27 -0700

    eod

--- src/components/FortuneTeller.js
+++ src/components/FortuneTeller.js
@@ -4,7 +4,7 @@ import './FortuneTeller.css';
 const fortunes = [
   'Be brave when facing the competition.',
   'An exciting opportunity lies ahead of you.',
-  'A routine task will turn into an exchanting adventure.',
+  'A routine task will turn into an enchanting adventure.',
   'Be true to your work, your word, and your friends.',
   'You will receive money from an unexpected source.',
   'Plan for many pleasures ahead.',
@@ -18,15 +18,15 @@ export default function FortuneTeller() {
   return (
     <>
       <div className="Fortune-Teller neushadow">
-        <div className="FT-Header neushadow">
+        <div className="FT-Header">
           <div className="FT-Title">Fortunes, Forcasts, Lucky Charms</div>
           <div className="FT-SubTitle">Catch the dope of your horoscope!</div>
         </div>
-        <div className="FT-Main">
-          <div className="FT-Panel">{fortune}</div>
-          <div className="FT-Button" onClick={() => getFortune()}>
-            Push me!
-          </div>
+        <div className="FT-Panel">
+          <p>{fortune}</p>
+        </div>
+        <div className="FT-Button" onClick={() => getFortune()}>
+          Push me!
         </div>
         <div className="FT-Footer">Please come again!</div>
       </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../css/flexbox.css';
 
 export default function Main() {
   return (
@@ -12,15 +13,66 @@ export default function Main() {
         This Single Page Application written in HTML5, CSS3, and JavaScript uses a custom configuration of React, Babel,
         & Webpack.
       </p>
-      <p className="code righted">
-        (HTML5 + CC3 + JavaScript)
-        <br />+ (ESLint + Web Standards)
-        <br />+ (React + Babel + Webpack)
-        <br />
-        _________________________
-        <br />
-        Single Page Application
-      </p>
+      <h3>Some Technologies With Which I Am Familiar</h3>
+      <div className="tech-icon-grid">
+        <div className="tech-icon">
+          <i className="fab fa-html5 fa-5x" />
+        </div>
+        <div className="tech-icon">
+          <i className="fab fa-css3-alt fa-5x" />
+        </div>
+        <div className="tech-icon">
+          <i className="fab fa-js-square fa-5x" />
+        </div>
+        <div className="tech-icon">
+          <i className="fab fa-react fa-5x" />
+        </div>
+        <div className="tech-icon">
+          <i className="fab fa-less fa-5x" />
+        </div>
+        <div className="tech-icon">
+          <i className="fab fa-sass fa-5x" />
+        </div>
+        <div className="tech-icon">
+          <i className="fab fa-sketch fa-5x" />
+        </div>
+        <div className="tech-icon">
+          <i class="fab fa-aws fa-5x"></i>
+        </div>
+        <div className="tech-icon">
+          <i class="fab fa-docker fa-5x"></i>
+        </div>
+        <div className="tech-icon">
+          <i class="fab fa-dropbox fa-5x"></i>
+        </div>
+        <div className="tech-icon">
+          <i class="fab fa-git-alt fa-5x"></i>
+        </div>
+        <div className="tech-icon">
+          <i class="fab fa-node-js fa-5x"></i>
+        </div>
+        <div className="tech-icon">
+          <i class="fab fa-npm fa-5x"></i>
+        </div>
+        <div className="tech-icon">
+          <i class="fab fa-python fa-5x"></i>
+        </div>
+        <div className="tech-icon">
+          <i class="fab fa-raspberry-pi fa-5x"></i>
+        </div>
+        <div className="tech-icon">
+          <i class="fab fa-apple fa-5x"></i>
+        </div>
+        <div className="tech-icon">
+          <i class="fab fa-microsoft fa-5x"></i>
+        </div>
+        <div className="tech-icon">
+          <i class="fab fa-linux fa-5x"></i>
+        </div>
+        <div className="tech-icon">
+          <i class="fab fa-ubuntu fa-5x"></i>
+        </div>
+      </div>
       <p>Please take a look around and enjoy!</p>
       <p>
         Sincerely,
