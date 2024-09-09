dccfd6bd40f50af32b7c8d2ee3721950d34f5474
John Sullivan <johnography@outlook.com>
2024-09-09 13:41:09 -0700

    Add canvas

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import Home from './Home';
-import FortuneTeller from './FortuneTeller';
-import DataVis from './DataVis';
+import React from "react";
+import Home from "./Home";
+import FortuneTeller from "./FortuneTeller";
+import DataVis from "./DataVis";
+import Canvas from "./Canvas";
 
 export default function Main() {
   return (
     <>
       <Home />
       <FortuneTeller />
+      <Canvas />
       <DataVis />
     </>
   );
